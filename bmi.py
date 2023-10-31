f5405f14409286c2f5ba2af40dc04a15a79f9fc0
dinesh <dinesh.22@ichat.sp.edu.sg>
2023-10-31 13:59:39 +0800

    bmi

--- bmi.py
+++ bmi.py
@@ -11,11 +11,14 @@ def calculate_bmi(height, weight):
     # Classify weight based on BMI range
     if bmi < 18.5:
         print("Weight Classification: Under Weight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("Weight Classification: Normal Weight")
+        return 0
     else:
         print("Weight Classification: Over Weight")
-
+        return 1
 
 # Example usage
-calculate_bmi(weight=57, height=1.73)
+classification_result = calculate_bmi(weight=57, height=1.73)
+print("Weight Classification Result:", classification_result)
